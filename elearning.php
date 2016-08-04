3908b26e952875836e5181a86fd52756d65a21ff
Lucas Ruelle <loiu92@gmail.com>
2016-08-04 23:59:32 +0200

    fixing create dir problem

--- elearning.php
+++ elearning.php
@@ -1,50 +1,44 @@
+#!/usr/bin/php
 <?php
+if (!isset($argv[1]))
+	{
+		echo "Veuillez mettre votre lien d'autologin\n";
+		return -1;
+	}
+else if ($argv[1] == "-h")
+{
+	echo "Usage: ./" . $argv[0] . " autologin_token (ex: auth-1a2b3c4d5e) [directory for files]\n";
+	return 0;
+}
+
 $lien_elearning = "https://intra.epitech.eu" . "/e-learning/?format=json";
-$lien_autologin = 'https://intra.epitech.eu/' . $argv[1];  // Mettez ici votre lien d'auto-login
+$lien_autologin = 'https://intra.epitech.eu/' . $argv[1];
 $script_dir = getcwd();
 
-$return = login_intra_json($lien_autologin, $lien_elearning);  // Fais une requête qui se connecte à l'intra.
-$intra = json_decode($return);					// Decode JSON le resultat de la 1ère requete.
+$return = login_intra_json($lien_autologin, $lien_elearning, $script_dir);
+$intra = json_decode($return);
 
-
-for ($nb_semester=0; $nb_semester < 4; $nb_semester++) {
+for ($nb_semester=4; $nb_semester < 5; $nb_semester++) {
 	$modules = list_modules_semester($intra, $nb_semester);
 	init_directory($script_dir);
 	init_directory_semester($nb_semester, $script_dir);
 	create_dir_modules($intra, $nb_semester, $modules, $script_dir);
 
 	foreach ($intra[$nb_semester]->modules as $working_module) {
-			chdir($script_dir . '/Modules/Semestre' . $nb_semester . '/' . $working_module->slug);
 			foreach ($working_module->classes as $classes) {
+				chdir($script_dir . '/Modules/Semestre' . $nb_semester . '/' . $working_module->slug);
 				mkdir($classes->slug);
 				chdir($classes->slug);
 				foreach ($classes->steps as $steps) {
-					$path_cookie = 'cookies_connexion.txt';
-					if (!file_exists(realpath($path_cookie))) touch($path_cookie);
-					$curl = curl_init();
-					curl_setopt($curl, CURLOPT_URL, $lien_autologin);
-					curl_setopt($curl, CURLOPT_COOKIESESSION, true);
-					curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-					curl_setopt($curl, CURLOPT_POST, false);
-					curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
-					curl_setopt($curl, CURLOPT_COOKIEJAR, realpath($path_cookie));
-					$return = curl_exec($curl);
-					$data_json = array();
-					curl_setopt($curl, CURLOPT_URL, $steps->step->fullpath);
-					curl_setopt($curl, CURLOPT_POST, false);
-					curl_setopt($curl, CURLOPT_POSTFIELDS, $data_json);
-					$content = curl_exec($curl);
-					curl_close($curl);
-					$name_content = substr($steps->step->fullpath, strripos($steps->step->fullpath, "/") + 1);
-					file_put_contents($name_content, $content);
+						get_file($steps, $script_dir, $lien_autologin);
 				}
 			};
 	}
 }
 
-function login_intra_json($lien_autologin, $lien_elearning)
+function login_intra_json($lien_autologin, $lien_elearning, $script_dir)
 {
-	$path_cookie = 'cookies_connexion.txt';
+	$path_cookie = $script_dir . '/cookies_connexion.txt';
 	if (!file_exists(realpath($path_cookie))) touch($path_cookie);
 	$curl = curl_init();
 	curl_setopt($curl, CURLOPT_URL, $lien_autologin);
@@ -86,6 +80,28 @@ function list_modules_semester($intra, $nb_semester)
 			$i = $i + 1;
 	}
 	return $modules_semester;
+}
+
+function get_file($steps, $script_dir, $lien_autologin)
+{
+	$path_cookie = $script_dir . '/cookies_connexion.txt';
+	if (!file_exists(realpath($path_cookie))) touch($path_cookie);
+	$curl = curl_init();
+	curl_setopt($curl, CURLOPT_URL, $lien_autologin);
+	curl_setopt($curl, CURLOPT_COOKIESESSION, true);
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($curl, CURLOPT_POST, false);
+	curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+	curl_setopt($curl, CURLOPT_COOKIEJAR, realpath($path_cookie));
+	$return = curl_exec($curl);
+	$data_json = array();
+	curl_setopt($curl, CURLOPT_URL, $steps->step->fullpath);
+	curl_setopt($curl, CURLOPT_POST, false);
+	curl_setopt($curl, CURLOPT_POSTFIELDS, $data_json);
+	$content = curl_exec($curl);
+	curl_close($curl);
+	$name_content = substr($steps->step->fullpath, strripos($steps->step->fullpath, "/") + 1);
+	file_put_contents($name_content, $content);
 }
 
 ?>
